f819c8da457ffb97597eecd05636e86f37262d89
peron17 <tommypriambodo@gmail.com>
2022-08-26 11:51:34 +0700

    adjust error page

--- src/resources/views/admin/error/401.blade.php
+++ src/resources/views/admin/error/401.blade.php
@@ -6,6 +6,6 @@
         <div class="error mx-auto" data-text="401">401</div>
         <p class="lead text-gray-800 mb-5">Not Authorized</p>
         <p class="text-gray-500 mb-0">Sorry, You can't access this page</p>
-        <a href="index.html">&larr; Back to Dashboard</a>
+        <a href="{{ route('admin.dashboard') }}">&larr; Back to Dashboard</a>
     </div>
 @endsection
--- src/resources/views/admin/layouts/_header.blade.php
+++ src/resources/views/admin/layouts/_header.blade.php
@@ -13,7 +13,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth('admin')->user()->name }}</span>
                 <img class="img-profile rounded-circle"
                     src="{{ asset('sbadmin/img/undraw_profile.svg') }}">
             </a>
@@ -33,7 +33,7 @@
                     Activity Log
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="{{ route('admin.logout') }}">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
